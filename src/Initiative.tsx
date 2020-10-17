e872326b7d7a11c7aed8fd81a55a48e56c1f483c
Trevor Fehrman <trevorfehrman@gmail.com>
2020-10-17 12:15:26 -0700

    edit and some conditional rendering and delete

--- src/Initiative.tsx
+++ src/Initiative.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { FC, useState } from 'react';
 import { motion } from 'framer-motion';
 import move from 'array-move';
 import nextId from 'react-id-generator';
@@ -14,27 +14,48 @@ const spring = {
   stiffness: 120,
 };
 
-
-export const Initiative = () => {
+export const Initiative: FC = () => {
   const [participants, setParticipants] = useState<IParticipant[]>([]);
-  
+
   function advanceInitiative(): void {
     setParticipants(move(participants, 0, -1));
   }
 
   function addParticipant(): void {
-    setParticipants([...participants, { id: nextId(), backgroundColor: 'grey' }]);
+    setParticipants([
+      ...participants,
+      { id: nextId(), backgroundColor: 'grey' },
+    ]);
   }
 
   function editParticipant(id: string, values: Partial<IParticipant>): void {
-    console.log(id, values);
+    setParticipants(
+      participants.map((participant) => {
+        if (participant.id === id) {
+          return {
+            ...participant,
+            ...values,
+          };
+        }
+        return participant;
+      })
+    );
+  }
 
+  function removeParticipant(id: string) {
+    setParticipants(
+      participants.filter((participant) => participant.id !== id)
+    );
   }
 
   return (
     <>
-      <button type="button" onClick={advanceInitiative}>Advance Initiative</button>
-      <button type="button" onClick={addParticipant}>Add Participant</button>
+      <button type="button" onClick={advanceInitiative}>
+        Advance Initiative
+      </button>
+      <button type="button" onClick={addParticipant}>
+        Add Participant
+      </button>
       <ul className={styles.initiativeList}>
         {participants.map((participant) => (
           <motion.li
@@ -46,6 +67,7 @@ export const Initiative = () => {
             <InitiativeParticipant
               participant={participant}
               editParticipant={editParticipant}
+              removeParticipant={removeParticipant}
             />
           </motion.li>
         ))}
@@ -53,4 +75,3 @@ export const Initiative = () => {
     </>
   );
 };
-
